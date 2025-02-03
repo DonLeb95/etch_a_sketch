729aedb52073c62f3955bf2a44c765c09e67662f
Donnie <82501498+DonLeb95@users.noreply.github.com>
2025-02-03 01:22:23 -0400

    Add a preventative measure in case of grid builder function error
    Still have to revise button setup, some issues showed up in initial tests, currently no button functionality

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
 let gridOfSquares = [];
 const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 0;i < 16 * 16; ++i){
-    gridOfSquares[i] = document.createElement("div");;
-    gridContainer.appendChild(gridOfSquares[i]);
-}
+function gridBuilder(sqrDim){
 
+    if(isNaN(Number(sqrDim) && sqrDim <= 100)){
+        alert("There must have been an error. Make sure you only wrote a number(1,22,33,87) and that it isnt higher than 100")
+        return gridBuilder(16)
+    }
+    for (let i = 0;i < sqrDim * sqrDim; ++i){
+        gridOfSquares[i] = document.createElement("div");;
+        gridContainer.appendChild(gridOfSquares[i]);
+        }
+    };
 
+gridBuilder(16);
+
+const gridDensity = document.querySelector(".grid-density");
+
+//gridDensity.addEventListener("click",);
