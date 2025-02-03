3eee77d01d34be5056a77d28aff0add993ab93e2
Donnie <82501498+DonLeb95@users.noreply.github.com>
2025-02-02 22:43:47 -0400

    Create 16 x 16 grid and set container dimesnions

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-const squareDiv = document.createElement("div");
-
 let gridOfSquares = [];
 const gridContainer = document.querySelector(".grid-container");
 
 for (let i = 0;i < 16 * 16; ++i){
-    gridOfSquares[i] = squareDiv;
+    gridOfSquares[i] = document.createElement("div");;
     gridContainer.appendChild(gridOfSquares[i]);
 }
 
