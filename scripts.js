b5a620558e7ab7f16093951c852b9a3b688dd2c6
Donnie <82501498+DonLeb95@users.noreply.github.com>
2025-02-04 23:56:43 -0400

    Create and size inner squares dynamically
    Did some alterations to gridBuilder function so that it may clear old grid and create and resize new inner squares

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
-let gridOfSquares = [];
-const gridContainer = document.querySelector(".grid-container");
-
 function gridBuilder(sqrDim){
-
-    if(isNaN(Number(sqrDim) && sqrDim <= 100)){
+    
+    if(isNaN(Number(sqrDim) || sqrDim > 100)){
         alert("There must have been an error. Make sure you only wrote a number(1,22,33,87) and that it isnt higher than 100")
         return gridBuilder(16)
     }
+    
+    const gridContainer = document.querySelector(".grid-container");
+    const gridOfSquares = [];
+
+    gridContainer.replaceChildren();
+
     for (let i = 0;i < sqrDim * sqrDim; ++i){
         gridOfSquares[i] = document.createElement("div");;
-        gridContainer.appendChild(gridOfSquares[i]);
-        }
+        gridContainer.appendChild(gridOfSquares[i]);    
+    }
+    
+    const gridContData = getComputedStyle(gridContainer);
+    const gridContHeight = gridContData.getPropertyValue("height");
+    const gridContWidth = gridContData.getPropertyValue("width");
+
+    const innerSqrDim = Number(gridContHeight.replace("px","")) / sqrDim +"px";
+    gridOfSquares.forEach((sqr) => sqr.setAttribute("style",
+        `height: ${innerSqrDim}; width: ${innerSqrDim};`))
     };
 
 gridBuilder(16);
